7ca498b7500b76fbc33dc586dd244ea5e17ef6ce
linzebin <443189390@qq.com>
2017-03-02 19:43:28 +0800

    增加个universal link为了测试

--- src/views/index.vue
+++ src/views/index.vue
@@ -4,7 +4,7 @@
         <div class="row-container">
             <a href="#" class="header"></a>
             <ul class="content-wrapper">
-                <li class="content-list">1</li>
+                <li class="content-list"><a href="applinks:hongtang.s1.natapp.cc">applinks</a></li>
                 <li class="content-list">2</li>
                 <li class="content-list">3</li>
                 <li class="content-list">4</li>
