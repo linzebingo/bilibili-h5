56f35bfafba6b80f67cf99fc9fcd0f0e663f3dff
linzebin <443189390@qq.com>
2017-03-27 22:21:32 +0800

    完成了番剧推荐页面

--- src/components/bangumiRecommend.ts
+++ src/components/bangumiRecommend.ts
@@ -5,17 +5,31 @@ import Api from '../api'
 @Component
 export default class BangumiRecommend extends Vue {
     showError = false
-    bangumiItems
+    loading = true
+    bangumiItems = new Array<BangumiInfo>()
     mounted() {
         this.fetcData();
     }
 
     fetcData() {
         Api.getBangumiList().then(response => {
-
+            this.loading = false
+            var jsonData = response.data as BaseListResponse<BangumiInfo>
+            if (jsonData && jsonData.code === 0) {
+                this.bangumiItems = jsonData.list.splice(0, 6)
+                this.delayImage()
+            } else {
+                this.showError = true
+            }
         }, response => {
-            console.error(response)
+            this.loading = false
             this.showError = true
+        })
+    }
+
+    delayImage() {
+        this.$nextTick(() => {
+
         })
     }
 }
@@ -27,5 +41,26 @@ interface BaseListResponse<T> {
     list: Array<T>
 }
 interface BangumiInfo {
-
+    area: string;
+    arealimit: number;
+    attention: number;
+    bangumi_id: number;
+    bgcount: number;
+    cover: string;
+    danmaku_count: number;
+    favorites: number;
+    is_finish: number;
+    lastupdate: number;
+    lastupdate_at: string;
+    new: boolean;
+    play_count: number;
+    pub_time: string;
+    season_id: number;
+    season_status: number;
+    spid: number;
+    square_cover: string;
+    title: string;
+    url: string;
+    viewRank: number;
+    weekday: number;
 }
--- src/views/index.vue
+++ src/views/index.vue
@@ -10,166 +10,181 @@
 <script lang="ts" src="./index.ts"></script>
 
 <style lang="scss">
-    .row-container {
-        position: relative;
-        width: 100%;
-        margin-bottom: 12px;
-        background-color: #fff;
-        box-sizing: border-box;
-        .header {
-            display: block;
-            height: 44px;
-            line-height: 44px;
-            padding: 0 12px;
-            .left {
-                float: left;
-                color: #444;
-                font-size: 14px;
-                position: relative;
-                padding-left: 25px;
-            }
-            .right {
-                float: right;
-                font-size: 14px;
-                color: #999;
-                position: relative;
-                padding-left: 25px;
-            }
-            .hot {
-                font-size: 13px;
-                color: #ffb30f;
-            }
-        }
-        * {
-            box-sizing: border-box;
-        }
-        .content-wrapper {
-            display: block;
-            width: 100%;
-            overflow: hidden;
-            padding: 0 6px;
-        }
-        .content-list {
-            width: 50%;
-            display: inline-block;
-            position: relative;
-            padding: 0 6px 24px;
-            vertical-align: top;
-            float: left;
-            .item {
-                display: block;
-                width: 100%;
-                height: 100%;
-                text-decoration: none;
-                text-overflow: ellipsis;
-                white-space: nowrap;
-                overflow: hidden;
-                .top {
-                    position: relative;
-                    background-repeat: no-repeat;
-                    background-position: center;
-                    width: 100%;
-                    height: 0;
-                    padding-bottom: 63%;
-                }
-                .cover-img {
-                    position: absolute;
-                    top: 0;
-                    left: 0;
-                    transition: opacity .3s;
-                    width: 100%;
-                    height: 100%;
-                    background-size: 100% 100%;
-                    background-repeat: no-repeat;
-                    background-position: center center;
-                    border-radius: 5px;
-                }
-            }
-            .info {
-                position: relative;
-                display: block;
-                width: 100%;
-                padding-top: 5px;
-                .title {
-                    width: 100%;
-                    max-height: 36px;
-                    line-height: 1.4;
-                    overflow: hidden;
-                    color: #444;
-                    font-size: 13px;
-                }
-                .cell {
-                    position: relative;
-                    width: 50%;
-                    font-size: 13px;
-                    color: #999;
-                    padding-left: 18px;
-                    text-overflow: ellipsis;
-                    overflow: hidden;
-                    white-space: nowrap;
-                }
-                .avator {
-                    position: relative;
-                    width: 42px;
-                    height: 42px;
-                    border-radius: 21px;
-                    border: 2px solid #fff;
-                    top: -26px;
-                    left: 7px;
-                    z-index: 10;
-                    img {
-                        width: 100%;
-                        height: 100%;
-                        border-radius: 50%;
-                        vertical-align: top;
-                    }
-                }
-                .user {
-                    position: absolute;
-                    left: 0;
-                    top: 5px;
-                    width: 100%;
-                    padding-left: 56px;
-                }
-                .name {
-                    font-size: 13px;
-                    color: #444;
-                }
-                .desp {
-                    font-size: 13px;
-                    color: #999;
-                    padding-top: 2px;
-                }
-                .online {
-                    position: absolute;
-                    top: 25px;
-                    width: 48px;
-                    padding: 0 1px;
-                    color: #fff;
-                    background-color: #ccc;
-                    border-radius: 6px;
-                    text-align: center;
-                    height: 20px;
-                    line-height: 20px;
-                    font-size: 12px;
-                }
-            }
-        }
+.row-container {
+    position: relative;
+    width: 100%;
+    margin-bottom: 12px;
+    background-color: #fff;
+    box-sizing: border-box;
+    .header {
+        display: block;
+        height: 44px;
+        line-height: 44px;
+        padding: 0 12px;
         .left {
             float: left;
+            color: #444;
+            font-size: 14px;
+            position: relative;
+            padding-left: 25px;
         }
-        .text-overflow {
-            text-overflow: ellipsis;
-            overflow: hidden;
-            white-space: nowrap;
+        .right {
+            float: right;
+            font-size: 14px;
+            color: #999;
+            position: relative;
+            padding-left: 25px;
+        }
+        .hot {
+            font-size: 13px;
+            color: #ffb30f;
         }
     }
-    
-    .clearfix::after {
-        content: "";
+    * {
+        box-sizing: border-box;
+    }
+    .content-wrapper {
         display: block;
-        height: 0;
-        clear: both;
-        visibility: hidden;
+        width: 100%;
+        overflow: hidden;
+        padding: 0 6px;
     }
+    .content-list {
+        width: 50%;
+        display: inline-block;
+        position: relative;
+        padding: 0 6px 24px;
+        vertical-align: top;
+        .item {
+            display: block;
+            width: 100%;
+            height: 100%;
+            text-decoration: none;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            overflow: hidden;
+            .top {
+                position: relative;
+                background-repeat: no-repeat;
+                background-position: center;
+                width: 100%;
+                height: 0;
+                padding-bottom: 63%;
+            }
+            .cover-img {
+                position: absolute;
+                top: 0;
+                left: 0;
+                transition: opacity .3s;
+                width: 100%;
+                height: 100%;
+                background-size: 100% 100%;
+                background-repeat: no-repeat;
+                background-position: center center;
+                border-radius: 5px;
+            }
+        }
+        .info {
+            position: relative;
+            display: block;
+            width: 100%;
+            padding-top: 5px;
+            .title {
+                width: 100%;
+                max-height: 36px;
+                line-height: 1.4;
+                overflow: hidden;
+                color: #444;
+                font-size: 13px;
+            }
+            .bangumi-title {
+                font-size: 13px;
+                text-align: center;
+                color: #444;
+            }
+            .bangumi-page {
+                text-align: center;
+                font-size: 12px;
+                color: #de698c;
+            }
+            .cell {
+                position: relative;
+                width: 50%;
+                font-size: 13px;
+                color: #999;
+                padding-left: 18px;
+                text-overflow: ellipsis;
+                overflow: hidden;
+                white-space: nowrap;
+            }
+            .avator {
+                position: relative;
+                width: 42px;
+                height: 42px;
+                border-radius: 21px;
+                border: 2px solid #fff;
+                top: -26px;
+                left: 7px;
+                z-index: 10;
+                img {
+                    width: 100%;
+                    height: 100%;
+                    border-radius: 50%;
+                    vertical-align: top;
+                }
+            }
+            .user {
+                position: absolute;
+                left: 0;
+                top: 5px;
+                width: 100%;
+                padding-left: 56px;
+            }
+            .name {
+                font-size: 13px;
+                color: #444;
+            }
+            .desp {
+                font-size: 13px;
+                color: #999;
+                padding-top: 2px;
+            }
+            .online {
+                position: absolute;
+                top: 25px;
+                width: 48px;
+                padding: 0 1px;
+                color: #fff;
+                background-color: #ccc;
+                border-radius: 6px;
+                text-align: center;
+                height: 20px;
+                line-height: 20px;
+                font-size: 12px;
+            }
+        }
+    }
+    .bangumi-list {
+        width: 33%;
+        .top {
+            padding-top: 75%;
+        }
+    }
+    .left {
+        float: left;
+    }
+    .text-overflow {
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+    }
+}
+
+.clearfix::after {
+    content: "";
+    display: block;
+    height: 0;
+    clear: both;
+    visibility: hidden;
+}
 </style>
