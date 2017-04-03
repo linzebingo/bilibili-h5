4d2db77af8aab1f053ae3d7bc4364f8a9176bbcb
linzebin <443189390@qq.con>
2017-04-03 20:55:25 +0800

    搜索栏

--- src/app.vue
+++ src/app.vue
@@ -9,14 +9,51 @@
             <nav class="nav-bar">
                 <div class="nav-wrp">
                     <router-link to="/index" class="logo"></router-link>
-                    <router-link to="/history" class="cell"><i class="nav-icon icon-history"></i></router-link>
-                    <router-link to="/index" class="cell" ><i class="nav-icon icon-search"></i></router-link>
+                    <router-link to="/history" class="cell"><i class="nav-icon icon-history"></i>历史</router-link>
+                    <a href="javascript:void(0);" class="cell" @click="show_search=true"><i class="nav-icon icon-search"></i>搜索</a>
                 </div>
             </nav>
         </div>
 
         <!-- search panel -->
-        <div class="search toggle-panel"></div>
+        <div class="search toggle-panel" v-show="show_search" >
+            <div class="toggle-panel-bg"></button></div>
+            <div class="toggle-panel-inner">
+                <div class="search-bar">
+                    <form action="">
+                        <input type="text" name="keyword" placeholder="搜索视频、番剧、up主或AV号">
+                    </form>
+                    <a class="search-cancel" @click="show_search=false">取消</a>
+                </div>
+                <div class="search-wrapper">
+                    <div class="hot-wrapper">
+                        <div class="title">
+                            <span class="line"></span>
+                            <span class="name">热门搜索</span>
+                            <span class="line"></span>
+                        </div>
+                        <div class="hot-content clearfix">
+                            <a class="hot-item">人渣的本愿</a>
+                            <a class="hot-item">大司马</a>
+                            <a class="hot-item">肥蛇传</a>
+                            <a class="hot-item">风味绝配</a>
+                            <a class="hot-item">极乐净土</a>
+                        </div>
+                    </div>
+                    <div class="history-wrapper">
+                        <div class="title">
+                            <span class="line"></span>
+                            <span class="name">历史搜索</span>
+                            <span class="line"></span>
+                        </div>
+                        <div class="history-content">
+                            <a class="history-item">极乐净土</a>
+                            <a class="history-item">高达</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         
         <!--<transition name="router-slide">-->
@@ -37,6 +74,7 @@
     })
     export default class App extends Vue {
         appload = false //结构页
+        show_search = false
         
         mounted(){
             this.appload = true
@@ -53,4 +91,7 @@
 	.router-slide-enter, .router-slide-leave-active {
 	  	transform: translateX(-100%);
 	}
+    #app {
+        min-height: 100%;
+    }
 </style>
